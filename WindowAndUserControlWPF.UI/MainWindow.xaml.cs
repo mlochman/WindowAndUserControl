202d39650d9eb192165553fac12c187258385654
Michael Lochman <mikelochman16@gmail.com>
2019-11-11 23:54:54 -0500

    Initial Commit

---
+++ WindowAndUserControlWPF.UI/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using WindowsAndUserControl.DataAccess;
+using WindowsAndUserControl.Model;
+
+namespace WindowAndUserControlWPF.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GetMovies_Click(object sender, RoutedEventArgs e)
+        {
+            using (var context = new MasterMovieDbContext())
+            {
+                IEnumerable<Movie> movieList = context.GetAllMovies();
+
+                foreach (var item in movieList)
+                {
+                    Console.WriteLine($"{item.Id}, {item.Name}, {item.DateReleased}, {item.GenreId}");
+                }
+            }
+        }
+    }
+}
---
+++ WindowsAndUserControl.Model/Movies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsAndUserControl.Model
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? DateReleased { get; set; }
+        public int GenreId { get; set; }
+
+    }
+}
